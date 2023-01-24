f8611357193c5179155078637d93e3d2773b4b37
Astarthean <asakalieu@gmail.com>
2023-01-24 12:58:27 +0100

    Actualización Lección_Nueva 2

--- src/com/ana/Leccion_Nueva.java
+++ src/com/ana/Leccion_Nueva.java
@@ -3,5 +3,6 @@ package com.ana;
 public class Leccion_Nueva {
     public static void main(String[] args) {
         System.out.printf("Hola");
+        System.out.printf("Segundo cambio");
     }
 }
